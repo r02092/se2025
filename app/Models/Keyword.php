98c650847d3792e76184821a5c9a82193b2204a6
Kumono <133235046+Kumonikumon@users.noreply.github.com>
2025-12-24 23:16:58 +0900

    Keyword.phpを作成

---
+++ app/Models/Keyword.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Keyword extends Model
+{
+    protected $casts = [
+        'id' => 'int',
+        'spot_id' => 'int',
+        'keyword' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime'
+    ];
+    protected $guarded = ['id'];
+    public function spot()
+    {
+        return $this->belongsTo(Spot::class);
+    }
+}
+
