428b912843df0681bea38882e9c66a15cb73865b
r02092 <87572333+r02092@users.noreply.github.com>
2025-12-25 22:54:42 +0900

    テスト用データベースに関する記述を追加

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:refresh');
+    }
 }
