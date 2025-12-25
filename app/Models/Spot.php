25e3fd3e20fc1c3cc73a30b4cd709e551e7089b9
r02092 <87572333+r02092@users.noreply.github.com>
2025-12-25 10:54:44 +0900

    モデルを修正

--- app/Models/Spot.php
+++ app/Models/Spot.php
@@ -8,8 +8,18 @@ class Spot extends Model
 {
     protected $casts = [
         'id' => 'int',
-        'spot_id' => 'int',
-        'keyword' => 'string',
+        'user_id' => 'int',
+        'plan' => 'int',
+        'type' => 'int',
+        'name' => 'string',
+        'lng' => 'int',
+        'lat' => 'int',
+        'postal_code' => 'int',
+        'addr_city' => 'int',
+        'addr_detail' => 'string',
+        'description' => 'string',
+        'img_ext' => 'string',
+        'stamp_key' => 'int',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
         'deleted_at' => 'datetime',
@@ -34,5 +44,21 @@ class Spot extends Model
     public function stamps()
     {
         return $this->hasMany(Stamp::class);
+    }
+    public function getLngAttribute($value)
+    {
+        return ($value * 360) / (2 ** 32 - 1) - 180;
+    }
+    public function getLatAttribute($value)
+    {
+        return ($value * 180) / (2 ** 32 - 1) - 90;
+    }
+    public function setLngAttribute($value)
+    {
+        $this->attributes['lng'] = (($value + 180) * (2 ** 32 - 1)) / 360;
+    }
+    public function setLatAttribute($value)
+    {
+        $this->attributes['lat'] = (($value + 90) * (2 ** 32 - 1)) / 180;
     }
 }
