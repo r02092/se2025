e47576b28783d5d9a0aa2bc2efdf88e5221fdbc6
270333n <191536367+270333n@users.noreply.github.com>
2025-12-28 12:16:47 +0900

    MC00の作成

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Spot;
 
 class SearchController extends Controller
 {
-    //
+    public function index()
+    {
+        // ① メソッドチェーンによるデータ取得
+        $spots = Spot::inRandomOrder()->take(6)->get();
+
+        // ② ビューの表示とデータの引き渡し
+        return view('root', compact('spots'));
+    }
 }
