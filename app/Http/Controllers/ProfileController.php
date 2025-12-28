d92ec9070adc9cefdf29314b826a3ceec48d6d0a
yamato092 <215453125+yamato092@users.noreply.github.com>
2025-12-28 06:37:12 +0000

    フォーマッタの実行結果を適用

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,6 @@ use App\Models\User;
 
 class ProfileController extends Controller
 {
-    
     public function show(Request $request)
     {
         // 1. 利用者モジュールを用いて、現在認証されている利用者の情報を読み込み
@@ -16,7 +15,7 @@ class ProfileController extends Controller
 
         // 2. プロフィール画面出力モジュール (MV04: profile.blade.php) を呼び出し
         return view('profile', [
-            'user' => $user
+            'user' => $user,
         ]);
     }
 }
