346d562bebea41d4ba18747f53e18c908eb3a802
yamato092 <215453125+yamato092@users.noreply.github.com>
2025-12-28 15:36:17 +0900

    MC08。誤ってビューに関するコードの記述も含んでしまっていたためその部分を削除しました。

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class ProfileController extends Controller
+{
+    
+    public function show(Request $request)
+    {
+        // 1. 利用者モジュールを用いて、現在認証されている利用者の情報を読み込み
+        $user = Auth::user();
+
+        // 2. プロフィール画面出力モジュール (MV04: profile.blade.php) を呼び出し
+        return view('profile', [
+            'user' => $user
+        ]);
+    }
+}
