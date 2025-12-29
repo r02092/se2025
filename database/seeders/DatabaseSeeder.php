2a1e91461725f37fda794db4da4633bcba30a282
r02092 <87572333+r02092@users.noreply.github.com>
2025-12-29 17:11:52 +0900

    テスト用のレコードを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,193 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Traits\TransformCoordTrait;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
+    use TransformCoordTrait;
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            'login_name' => 'share_admin',
+            'password' => password_hash('password', PASSWORD_ARGON2ID),
+            'permission' => 0,
+            'name' => 'Share',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
+        DB::table('spots')->insert([
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 0,
+                'name' => 'chimney',
+                'lng' => $this->encodeLng(133.676274),
+                'lat' => $this->encodeLat(33.583968),
+                'postal_code' => 7820024,
+                'addr_city' => 39212,
+                'addr_detail' => '土佐山田町神通寺346-2',
+                'description' =>
+                    '人気絵本「パンどろぼう」のモチーフになったとされる、土佐山田のパン屋。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 6,
+                'name' => 'ごめん・なはり線',
+                'lng' => $this->encodeLng(133.906497),
+                'lat' => $this->encodeLat(33.504458),
+                'postal_code' => 7820010,
+                'addr_city' => 39203,
+                'addr_detail' => '東浜294',
+                'description' =>
+                    '南国市後免町と安芸郡奈半利町を結ぶ、2002年に開業した第三セクター路線。JR四国の路線と接続している。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 6,
+                'name' => 'とさでん交通（電車）',
+                'lng' => $this->encodeLng(133.54293),
+                'lat' => $this->encodeLat(33.559524),
+                'postal_code' => 7800833,
+                'addr_city' => 39201,
+                'addr_detail' => '南はりまや町1丁目',
+                'description' =>
+                    '南国市・高知市・吾川郡いの町を結ぶ路面電車。高知県唯一の電車でもある。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 2,
+                'name' => 'はりまや橋',
+                'lng' => $this->encodeLng(133.542639),
+                'lat' => $this->encodeLat(33.559944),
+                'postal_code' => 7800822,
+                'addr_city' => 39201,
+                'addr_detail' => 'はりまや町1丁目',
+                'description' =>
+                    '高知市の中心部にある橋。日本三大がっかり名所の一つとして知られる。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 0,
+                'name' => 'ひばり食堂',
+                'lng' => $this->encodeLng(133.663903),
+                'lat' => $this->encodeLat(33.764472),
+                'postal_code' => 7890312,
+                'addr_city' => 39344,
+                'addr_detail' => '高須226',
+                'description' =>
+                    'デカ盛りの聖地として知られる食堂。メディアに取り上げられることも多く、全国的に有名。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => 1,
+                'plan' => 1,
+                'type' => 1,
+                'name' => 'ひろめ市場',
+                'lng' => $this->encodeLng(133.535937),
+                'lat' => $this->encodeLat(33.560116),
+                'postal_code' => 7800841,
+                'addr_city' => 39201,
+                'addr_detail' => '帯屋町2丁目3-1',
+                'description' =>
+                    '飲食店や土産物店が軒を連ねる市場。観光スポットとしても、地元民の飲みの場としても人気。',
+                'img_ext' => 'jpg',
+                'stamp_key' => $this->randStampKey(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+        DB::table('keywords')->insert([
+            [
+                'spot_id' => 1,
+                'keyword' => 'パンどろぼう',
+            ],
+            [
+                'spot_id' => 2,
+                'keyword' => 'ラスト・ソング・フォー・ユー',
+            ],
+            [
+                'spot_id' => 3,
+                'keyword' => 'ラスト・ソング・フォー・ユー',
+            ],
+            [
+                'spot_id' => 3,
+                'keyword' => '海がきこえる',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => 'The Harimaya Bridge はりまや橋',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => 'たいようのマキバオー',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => 'クッキンアイドル アイ！マイ！まいん！',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => 'ゴリパラ見聞録',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => '南国土佐を後にして',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' => '海がきこえる',
+            ],
+            [
+                'spot_id' => 4,
+                'keyword' =>
+                    '特捜戦隊デカレンジャー 20th ファイヤーボール・ブースター',
+            ],
+            [
+                'spot_id' => 5,
+                'keyword' => '所さんの学校では教えてくれないそこんトコロ！',
+            ],
+            [
+                'spot_id' => 6,
+                'keyword' => 'おへんろ。',
+            ],
+            [
+                'spot_id' => 6,
+                'keyword' => '東海オンエア',
+            ],
+        ]);
+    }
+    private function randStampKey()
+    {
+        return rand(0, PHP_INT_MAX);
     }
 }
