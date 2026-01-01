9dc914d4ef282f34e548bffea730116833c6916d
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-01 21:06:07 +0900

    UGC監視・管理画面構成モジュールを作成

---
+++ app/Http/Controllers/AdminUgcController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AdminUgcRequest;
+use App\Models\Review;
+use App\Models\Photo;
+use DB;
+
+class AdminUgcController extends Controller
+{
+    const DISPLAY_NUM = 20;
+    public function get($page)
+    {
+        $ugcs = array_map(
+            function ($item) {
+                $id = $item['id'];
+                return ($item['type'] !== 'photo'
+                    ? Review::find($id)
+                    : Photo::find($id)
+                )->toArray();
+            },
+            Review::select(DB::raw('"review" as type'), 'id', 'updated_at')
+                ->union(
+                    Photo::select(
+                        DB::raw('"photo" as type'),
+                        'id',
+                        'updated_at',
+                    ),
+                )
+                ->orderBy('updated_at', 'desc')
+                ->offset($page * self::DISPLAY_NUM)
+                ->limit(self::DISPLAY_NUM)
+                ->get()
+                ->toArray(),
+        );
+        return response(''); // 仮
+    }
+    public function post(AdminUgcRequest $request)
+    {
+        $id = $request->input('id');
+        ($request->input('type') !== 'photo'
+            ? Review::find($id)
+            : Photo::find($id)
+        )->delete();
+        return redirect()->back();
+    }
+}
