4c8df236d9d95d7803f06e0fdb637b5d692bbbdf
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-02 23:25:59 +0900

    APIキー発行のモジュールのテストケースを追加

---
+++ tests/Unit/ApiKeyTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Controllers\ApiKeyController;
+use App\Http\Requests\ApiKeyRequest;
+use App\Models\User;
+
+class ApiKeyTest extends TestCase
+{
+    public function test_発行(): void
+    {
+        $controller = new ApiKeyController();
+        $this->assertEquals(200, $controller->get()->getStatusCode());
+        $this->assertEquals(
+            200,
+            $controller
+                ->post(
+                    new ApiKeyRequest([
+                        'create_name' => 'テスト',
+                    ]),
+                )
+                ->getStatusCode(),
+        );
+        $this->assertEquals(
+            200,
+            $controller
+                ->post(
+                    new ApiKeyRequest([
+                        'delete_id' => 1,
+                    ]),
+                )
+                ->getStatusCode(),
+        );
+        $this->assertEquals(
+            404,
+            $controller
+                ->post(
+                    new ApiKeyRequest([
+                        'delete_id' => 9,
+                    ]),
+                )
+                ->getStatusCode(),
+        );
+        $this->assertEquals(
+            400,
+            $controller->post(new ApiKeyRequest([]))->getStatusCode(),
+        );
+    }
+}
