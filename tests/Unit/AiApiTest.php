7a269f8cfde5ae28467b725159fae1628a1192cf
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-04 01:33:01 +0900

    AIスポット推薦処理モジュールでスポットが1つしか入力されていない場合に対応

--- tests/Unit/AiApiTest.php
+++ tests/Unit/AiApiTest.php
@@ -10,14 +10,26 @@ class AiApiTest extends TestCase
 {
     public function test_スポット推薦(): void
     {
+        $controller = new AiApiController();
         $this->assertEquals(
             200,
-            (new AiApiController())
+            $controller
                 ->post(
                     new AiApiRequest([
                         'chat' => 'この間にある観光スポットを推薦して',
                         'from' => 2,
                         'to' => 6,
+                    ]),
+                )
+                ->getStatusCode(),
+        );
+        $this->assertEquals(
+            400,
+            $controller
+                ->post(
+                    new AiApiRequest([
+                        'chat' => 'この周辺にある観光スポットを推薦して',
+                        'from' => 2,
                     ]),
                 )
                 ->getStatusCode(),
