a44cce077e9ee0b72743995d756ffc2daad4f808
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-04 00:38:49 +0900

    MU11の処理コード記述

--- app/Http/Controllers/SearchApiController.php
+++ app/Http/Controllers/SearchApiController.php
@@ -2,4 +2,39 @@
 
 namespace App\Http\Controllers;
 
-class SearchApiController extends Controller {}
+use APP\Models\Spot;
+use APP\Models\Keyword;
+use Illuminate\Http\Request;
+
+class SearchApiController extends Controller
+{
+    public function getSpotList(Request $request)
+    {
+        // クライアントからのキーワードとカテゴリの入力を取得
+        // inputの引数は仮
+        $keyword = $request->input('keyword');
+        $type = $request->input('type');
+
+        // カテゴリとキーワードの両方が指定されていない場合、エラーレスポンスを返す
+        if (!$keyword && !$type) {
+            return response()->json(
+                [
+                    'error' =>
+                        'キーワードまたはカテゴリのいずれかを指定してください。',
+                ],
+                400,
+            );
+        }
+
+        $spots = Spot::where('type', $type)->get();
+
+        //キーワードが指定されている場合、キーワードに基づいてフィルタリング
+        if ($keyword) {
+            $spots = $spots->filter(function ($spot) {
+                return $spot->keywords->contains('keyword', $keyword);
+            });
+        }
+
+        return response()->json($spots);
+    }
+}
