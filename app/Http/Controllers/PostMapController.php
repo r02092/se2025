8fdeb26d7c5f32bf068bfdc2348a503f0e380006
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-04 22:43:37 +0900

    単に投稿データを埋め込んだビューを返す処理を追加

--- app/Http/Controllers/PostMapController.php
+++ app/Http/Controllers/PostMapController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Photo;
 
 class PostMapController extends Controller
 {
-    //
+    function get()
+    {
+        $posts = Photo::all();
+        return view('post', ['posts' => $posts]);
+    }
 }
