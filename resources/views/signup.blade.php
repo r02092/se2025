a9728155f54e5f2c80d937ad8ff0d2bf9a997103
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-06 15:21:09 +0900

    不適切なHTMLタグを置き換え

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -26,7 +26,7 @@
 
 			<button type="submit">アカウント作成</button>
 		</form>
-		<h5>既にアカウントをお持ちの方は <a href="login.html">こちら</a></h5>
+		<div class="h5">既にアカウントをお持ちの方は <a href="login.html">こちら</a></div>
 	</div>
 </div>
 @endsection
