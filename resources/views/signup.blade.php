d69dbd23ff54285d44dd5db165f2e1ba96d05069
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-06 16:29:44 +0900

    アカウント作成画面で入力する項目についての説明を追加

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -8,13 +8,13 @@
 		<h2>アカウント作成</h2>
 		<form method="POST">
 			@csrf
-			<label for="name">表示名</label>
+			<label for="name">表示名<span class="form-detail">（1文字以上255文字以下の文字列）</span></label>
 			<input type="text" id="name" name="name" required />
 
-			<label for="username">ログイン名</label>
+			<label for="username">ログイン名<span class="form-detail">（半角英数字およびアンダーバーから構成される1文字以上255文字以下の文字列）</span></label>
 			<input type="text" id="username" name="username" required />
 
-			<label for="password">パスワード</label>
+			<label for="password">パスワード<span class="form-detail">（8文字以上999文字以下の文字列）</span></label>
 			<input type="password" id="password" name="password" required />
 
 			<label for="password_confirm">パスワード（確認用）</label>
