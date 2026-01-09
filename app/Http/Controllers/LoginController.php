60e92ba5efdbb110fcb02d809f3ac15723036681
Kumono <133235046+Kumonikumon@users.noreply.github.com>
2026-01-09 10:54:04 +0900

    ルート名を再び変更

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -57,6 +57,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('root');
     }
 }
