4d3e2fabfdd6b544253a0d188780a6b026ede21f
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-15 01:37:04 +0900

    クロージャに外部の変数を持ち込むように修正

--- app/Http/Controllers/SearchApiController.php
+++ app/Http/Controllers/SearchApiController.php
@@ -19,14 +19,14 @@ class SearchApiController extends Controller
 
         // カテゴリが指定されている場合、カテゴリに基づいてフィルタリング
         if (isset($type)) {
-            $spots = $spots->filter(function ($spot) {
+            $spots = $spots->filter(function ($spot) use ($type) {
                 return $spot->type === $type;
             });
         }
 
         //キーワードが指定されている場合、キーワードに基づいてフィルタリング
         if (isset($keyword)) {
-            $spots = $spots->filter(function ($spot) {
+            $spots = $spots->filter(function ($spot) use ($keyword) {
                 return $spot->keywords->contains('keyword', $keyword);
             });
         }
