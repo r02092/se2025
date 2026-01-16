74503a50f5527a685c06a766489cca4f4a84cd5d
Kumono <133235046+Kumonikumon@users.noreply.github.com>
2026-01-16 18:41:25 +0900

    構文エラーの修正

--- database/migrations/____add_google_id_to_users_table.php
+++ database/migrations/____add_google_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('google_id')->nullable();->after('id');
+            $table->string('google_id')->nullable()->after('id');
             $table->string('password')->nullable()->change();
         });
     }
