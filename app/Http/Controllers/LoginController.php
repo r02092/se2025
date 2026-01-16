beee41920f2b4b685891ccd1cbc2e59aed565a72
Kumono <133235046+Kumonikumon@users.noreply.github.com>
2026-01-16 16:11:20 +0900

    Google認証機能を追加し、ユーザーのgoogle_idをデータベースに保存するマイグレーションを作成

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -48,6 +50,58 @@ class LoginController extends Controller
 
         // 6. 通常ログイン完了
         return redirect()->intended(route('home'));
+    }
+
+    // Google 認証ページへリダイレクト 
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $socialUser = Socialite::driver('google')->user();
+        } catch (\Exception $e) {
+            return redirect()->route('login')->withErrors(['login_name' => 'Googleログインに失敗しました。']);
+        }
+
+        $user = User::where('email', $soclialUser->getEmail())
+            ->orWhere('google_ud', $socialUser->getId())
+            ->first();
+
+        if ($user) {
+            if (empty($user->google_id)) {
+                $user->google_id = $socialUser->getId();
+                $user->save();
+            }
+            Auth::login($user);
+        } else {
+            $user = USer::create([
+                'name' => $socialUser->getName(),
+                'email' => $socialUser->getEmail(),
+                'login_name' => $socialUser->getEmail(),
+                'google_id' => $socialUser->getId(),
+                'password' => password_hash(
+                    Str::radom(16),
+                    PASSWORD_ARGON2ID,
+                ),
+                'provider' => 1,
+            ]);
+            Auth::login($user);
+        }
+        session()->regenerate();
+        return $this->handlePostLogin($user);
+    }
+
+    private function handlePostLogin($user)
+    {
+        if (!empty($user->totp_secret)) {
+            session(['auth.2fa_required' => true]);
+            return redirect()->route('2fa.index');
+        }
+
+        return redirect()->route('home');
     }
 
     // ログアウト確認ページ表示
