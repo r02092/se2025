bf59f6e53789654e3170df16e34d71e4d6f3653e
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-20 08:39:04 +0900

    事業者申込フォームに郵便番号から住所を自動で入力するボタンを設置

--- resources/ts/subscription.ts
+++ resources/ts/subscription.ts
@@ -1,6 +1,29 @@
 document.getElementById("pref_select")?.addEventListener("change", e => {
 	cityFilter(e.currentTarget as HTMLSelectElement);
 });
+document.getElementById("pc2addrbtn")?.addEventListener("click", async () => {
+	const addr = await (
+		await fetch(
+			"/addr/" +
+				(document.getElementById("post_code") as HTMLInputElement).value,
+		)
+	).json();
+	[
+		{id: "pref_select", value: Math.floor(addr.city / 1000)},
+		{id: "city_select", value: addr.city},
+	].forEach(i => {
+		for (const j of (document.getElementById(i.id) as HTMLSelectElement)
+			.options) {
+			if (j.value === i.value.toString()) {
+				j.selected = true;
+			}
+		}
+	});
+	(document.getElementById("address") as HTMLInputElement).value = addr.addr;
+	if (!addr.city) {
+		alert("郵便番号から住所を推測できませんでした。");
+	}
+});
 cityFilter(document.getElementById("pref_select") as HTMLSelectElement);
 function cityFilter(selectElem: HTMLSelectElement) {
 	const pref = Number(selectElem.value);
