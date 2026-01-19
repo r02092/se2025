c00c956d9ed50cb0f92e6f03b82ac9ff50448ac3
S2666 <184213866+S2666@users.noreply.github.com>
2026-01-19 15:22:06 +0900

    事業者用メニューのUIも変更

--- resources/views/business.blade.php
+++ resources/views/business.blade.php
@@ -5,12 +5,27 @@
 @section('content')
 <h1>事業者機能</h1>
 
-<div class="general-box" style="margin: 0 5% 0">
-	<ul>
-		<li><a href="{{ route('spot.edit') }}">スポット情報編集</a></li>
-		<li><a href="{{ route('data') }}">観光データ確認</a></li>
-		<li><a href="#">APIキー管理</a></li>
-		<li><a href="#">請求書[PDF]</a></li>
-	</ul>
+<div class="general-box column-menu">
+    <h2>事業者メニュー</h2>
+    <div class="spot-divider" aria-hidden="true"></div>
+
+	<!-- <div class="column-menu"> -->
+		<button class="settings-button" onclick="location.href = '{{ route('spot.edit') }}'">
+			スポット情報編集
+		</button>
+
+		<button class="settings-button" onclick="location.href = '{{ route('data') }}'">
+			観光データ確認
+		</button>
+
+		<!-- 以下 遷移先空白 -->
+		<button class="settings-button" onclick="location.href = '#'">
+			APIキー管理
+		</button>
+
+		<button class="settings-button" onclick="location.href = '#'">
+			請求書[PDF]
+		</button>
+	</div>
 </div>
 @endsection
