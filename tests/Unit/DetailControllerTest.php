aaef8ddf671d2af370e78c45770df22ed080a1c4
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-21 01:44:01 +0900

    MU20のテストケース作成

---
+++ tests/Unit/DetailControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class DetailControllerTest extends TestCase
+{
+    public function test_create_screen_param_not_null(): void
+    {
+        $response = $this->get('/detail?id=1');
+
+        // ステータスコードが正常か
+        $response->assertStatus(200);
+        // 正しいBladeファイルを使っているか
+        $response->assertViewIs('spot-detail');
+        // Viewにpostsという変数が渡されているか
+        $response->assertViewHas('spot');
+    }
+
+    public function test_create_screen_param_null(): void
+    {
+        $response = $this->get('/detail');
+
+        // ステータスコードがNot Foundか
+        $response->assertStatus(404);
+    }
+}
