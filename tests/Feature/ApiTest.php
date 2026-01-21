1b7dff07c8dfdd39327aeda7aef05ee547b3ce74
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-21 19:34:35 +0900

    APIの結合テストを追加

---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+    public function test_api(): void
+    {
+        $this->get(route('api'), [
+            'Authorization' =>
+                'Bearer R9AnJFbw34/CWEQhwBVzDC4BGiAfM6IigWoU5pKm9L7y3zh6mzKEB32KjcyhPdq.d6hNo6+Iykh0EOVHQ/Wyp',
+        ])->assertStatus(200);
+        $this->get(
+            route('api', [
+                'from_date' => '2026-01-15',
+            ]),
+            [
+                'Authorization' =>
+                    'Bearer R9AnJFbw34/CWEQhwBVzDC4BGiAfM6IigWoU5pKm9L7y3zh6mzKEB32KjcyhPdq.d6hNo6+Iykh0EOVHQ/Wyp',
+            ],
+        )->assertStatus(200);
+        $this->get(
+            route('api', [
+                'to_date' => '2026-01-15',
+            ]),
+            [
+                'Authorization' =>
+                    'Bearer R9AnJFbw34/CWEQhwBVzDC4BGiAfM6IigWoU5pKm9L7y3zh6mzKEB32KjcyhPdq.d6hNo6+Iykh0EOVHQ/Wyp',
+            ],
+        )->assertStatus(200);
+        $this->get(
+            route('api', [
+                'from_date' => '2026-01-15',
+                'to_date' => '2026-01-15',
+            ]),
+            [
+                'Authorization' =>
+                    'Bearer R9AnJFbw34/CWEQhwBVzDC4BGiAfM6IigWoU5pKm9L7y3zh6mzKEB32KjcyhPdq.d6hNo6+Iykh0EOVHQ/Wyp',
+            ],
+        )->assertStatus(200);
+        $this->get(
+            route('api', [
+                'from_date' => 1,
+            ]),
+            [
+                'Authorization' =>
+                    'Bearer R9AnJFbw34/CWEQhwBVzDC4BGiAfM6IigWoU5pKm9L7y3zh6mzKEB32KjcyhPdq.d6hNo6+Iykh0EOVHQ/Wyp',
+            ],
+        )->assertStatus(302);
+        $this->get(route('api'), [
+            'Authorization' => 'Bearer invalid_api_key',
+        ])->assertStatus(401);
+    }
+}
