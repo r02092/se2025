943860e774d7d37d9ecc78fd6131c63efe9206bb
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-26 16:48:35 +0900

    投稿画面生成モジュールにおいてページの生成とスポットの取得を分離してメソッドを定義

--- app/Http/Controllers/PostMapController.php
+++ app/Http/Controllers/PostMapController.php
@@ -2,14 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Photo;
+use App\Models\User;
 
 class PostMapController extends Controller
 {
-    function index()
+    public function index()
     {
-        // 仮に全部送信
-        $posts = Photo::all();
-        return view('photo', ['posts' => $posts]);
+        return view('photo');
+    }
+
+    public function getPosts($request Request)
+    {
+        $cner_ne_lat = $request->query('ne_lat');
+        $cner_ne_lng = $request->query('ne_lng');
+        $cner_sw_lat = $request->query('sw_lat');
+        $cner_sw_lng = $request->query('sw_lng');
+
+        $photos = Photo::with('user')->all();
+
+        $photos = $photos->filter(function ($photo) use ($corner_ne, $corner_sw) {
+            $lat = $photo->get('lat');
+            $lng = $photo->get('lng');
+
+            return $lat <= $cner_ne_lat && $lat >= $cner_sw_lat && $lng <= $cner_ne_lng && $lng >= $cner_sw_lng;
+        });
+
+        $photos = $photos->map(function ($photo ) {
+            $user = $photo->user;
+            return [
+                'username' => $user->get('name'),
+                'avatar_url' => Strage::url(
+                    '/app/public/icons' .
+                        $user->get('id') .
+                        '.' .
+                        $user->get('img_ext'),
+                ),
+                'photo_image_url' => Strage::url(
+                    '/posts/' .
+                        $photo->get('id') .
+                        '.' .
+                        $photo->get('img_ext'),
+                ),
+                'comment' => $photo->get('comment'),
+                'created_at' => $photo->get('created_at'),
+                'updated_at' => $photo->get('updated_at'),
+            ];
+        });
+
+        return response()->json($photos->toArray());
     }
 }
