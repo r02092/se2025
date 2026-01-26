ef2a31b641cdbb9aa2e453ae125b7913fe8cb315
Kumono <133235046+Kumonikumon@users.noreply.github.com>
2026-01-26 13:25:12 +0900

    新規登録した利用者も利用者一覧に表示されるように修正

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,60 +5,29 @@
 @section('content')
 <h1>ユーザー一覧</h1>
 <section class="coupon-list">
+	@foreach($users as $user)
 	<article class="general-box coupon-card" tabindex="0" role="article">
-		<img class="coupon-thumb" src="{{ asset('images/Profile_pic.JPG') }}">
+		<img class="coupon-thumb" src="{{ $user->icon_url }}">
 		<div class="coupon-info">
-			<h2 class="coupon-title">Share</h2>
+			<h2 class="coupon-title">{{ $user->name }}</h2>
 			<p class="coupon-desc">
-				ID: 1<br>
-				ログイン名: share_admin<br>
-				種別: 管理者<br>
+				ID: {{ $user->id }}<br>
+				ログイン名: {{ $user->login_name }}<br>
+				種別: 
+				@if($user->permission === 0)
+					管理者
+				@elseif($user->permission === 1)
+					利用者
+				@elseif($user->permission === 2)
+					承認済み事業者
+				@else
+					その他
+				@endif
+				<br>
 			</p>
 		</div>
 	</article>
-	<article class="general-box coupon-card" tabindex="0" role="article">
-		<img class="coupon-thumb" src="{{ asset('images/Harimaya_Bridge.jpg') }}">
-		<div class="coupon-info">
-			<h2 class="coupon-title">はりまや</h2>
-			<p class="coupon-desc">
-				ID: 2<br>
-				ログイン名: yabashi_harimaaaa<br>
-				種別: 利用者<br>
-			</p>
-		</div>
-	</article>
-	<article class="general-box coupon-card" tabindex="0" role="article">
-		<img class="coupon-thumb" src="{{ asset('images/Profile_3.jpg') }}">
-		<div class="coupon-info">
-			<h2 class="coupon-title">中華そば 碧空</h2>
-			<p class="coupon-desc">
-				ID: 3<br>
-				ログイン名: hekiku_ramen<br>
-				種別: 承認済み事業者<br>
-			</p>
-		</div>
-	</article>
-	<article class="general-box coupon-card" tabindex="0" role="article">
-		<img class="coupon-thumb" src="{{ asset('images/Profile_4.jpg') }}">
-		<div class="coupon-info">
-			<h2 class="coupon-title">ツル☆ハシ</h2>
-			<p class="coupon-desc">
-				ID: 4<br>
-				ログイン名: tsuruhashi04<br>
-				種別: 利用者<br>
-			</p>
-		</div>
-	</article>
-	<article class="general-box coupon-card" tabindex="0" role="article">
-		<img class="coupon-thumb" src="{{ asset('images/Profile_5.jpg') }}">
-		<div class="coupon-info">
-			<h2 class="coupon-title">柳 勇樹</h2>
-			<p class="coupon-desc">
-				ID: 5<br>
-				ログイン名: yukiyanagi<br>
-				種別: 利用者<br>
-			</p>
-		</div>
-	</article>
+	@endforeach
+	{{ $users->links() }}
 </section>
 @endsection
