33b5abf8dcb45731f7c1b81eed68c595f0fe695b
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-27 22:16:18 +0900

    コーディングミスの修正とカラムへメンバからアクセスするように統一

--- app/Http/Controllers/PostMapController.php
+++ app/Http/Controllers/PostMapController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Photo;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class PostMapController extends Controller
 {
@@ -13,41 +14,45 @@ class PostMapController extends Controller
         return view('photo');
     }
 
-    public function getPosts($request Request)
+    public function getPosts(Request $request)
     {
         $cner_ne_lat = $request->query('ne_lat');
         $cner_ne_lng = $request->query('ne_lng');
         $cner_sw_lat = $request->query('sw_lat');
         $cner_sw_lng = $request->query('sw_lng');
 
-        $photos = Photo::with('user')->all();
+        $photos = Photo::with('user')->get();
 
-        $photos = $photos->filter(function ($photo) use ($corner_ne, $corner_sw) {
-            $lat = $photo->get('lat');
-            $lng = $photo->get('lng');
+        $photos = $photos->filter(function ($photo) use (
+            $cner_ne_lat,
+            $cner_ne_lng,
+            $cner_sw_lat,
+            $cner_sw_lng,
+        ) {
+            $lat = $photo->lat;
+            $lng = $photo->lng;
 
-            return $lat <= $cner_ne_lat && $lat >= $cner_sw_lat && $lng <= $cner_ne_lng && $lng >= $cner_sw_lng;
+            return $lat <= $cner_ne_lat &&
+                $lat >= $cner_sw_lat &&
+                $lng <= $cner_ne_lng &&
+                $lng >= $cner_sw_lng;
         });
 
-        $photos = $photos->map(function ($photo ) {
+        $photos = $photos->map(function ($photo) {
             $user = $photo->user;
             return [
-                'username' => $user->get('name'),
-                'avatar_url' => Strage::url(
-                    '/app/public/icons/' .
-                        $user->get('id') .
-                        '.' .
-                        $user->get('img_ext'),
+                'username' => $user->name,
+                'avatar_url' => Storage::url(
+                    'app/public/icons/' . $user->id . '.' . $user->icon_ext,
                 ),
-                'photo_image_url' => Strage::url(
-                    '/posts/' .
-                        $photo->get('id') .
-                        '.' .
-                        $photo->get('img_ext'),
+                'photo_img_url' => Storage::url(
+                    'posts/' . $photo->id . '.' . $photo->img_ext,
                 ),
-                'comment' => $photo->get('comment'),
-                'created_at' => $photo->get('created_at')->timestamp,
-                'updated_at' => $photo->get('updated_at')->timestamp,
+                'comment' => $photo->comment,
+                'lat' => $photo->lat,
+                'lng' => $photo->lng,
+                'created_at' => $photo->created_at->timestamp,
+                'updated_at' => $photo->updated_at->timestamp,
             ];
         });
 
