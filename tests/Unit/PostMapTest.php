4fcfa4466c9d5b56ca13a7b0978713eec092cfa7
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-01-27 22:41:50 +0900

    投稿画面用の簡単なテストケースを作成

--- tests/Unit/PostMapTest.php
+++ tests/Unit/PostMapTest.php
@@ -14,7 +14,14 @@ class PostMapTest extends TestCase
         $response->assertStatus(200);
         // 正しいBladeファイルを使っているか
         $response->assertViewIs('photo');
-        // Viewにpostsという変数が渡されているか
-        $response->assertViewHas('posts');
+    }
+
+    public function test_load_posts(): void
+    {
+        $response = $this->get(
+            '/post/load?sw_lat=30.0&sw_lng=130.0&ne_lat=40.0&ne_lng=135.0',
+        );
+
+        $response->assertStatus(200);
     }
 }
