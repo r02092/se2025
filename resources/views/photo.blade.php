27638c6351b76e159b0f7b66f1d92a637a938480
r02092 <87572333+r02092@users.noreply.github.com>
2026-01-29 00:32:48 +0900

    投稿作成ボタンの"+"の位置がフォントによってずれるのでCSSで表現するように変更

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -11,5 +11,5 @@
 <div class="gradient-top"></div>
 
 <div id="map" style="padding: 0; height: calc(100vh - (var(--header-height) + var(--bottom-bar-height)));"></div>
-<a href="{{ route('post.form') }}" class="photo-btn">+</a>
+<a href="{{ route('post.form') }}" class="photo-btn"></a>
 @endsection
