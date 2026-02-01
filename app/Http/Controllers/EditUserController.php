e4d6fe1b93d53dda164314820cc997a3f350b57c
Take-Noko <153985663+Take-Noko@users.noreply.github.com>
2026-02-02 03:53:34 +0900

    アイコンの保存処理を追加

--- app/Http/Controllers/EditUserController.php
+++ app/Http/Controllers/EditUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * MA05: 利用者編集画面構成モジュール
@@ -29,7 +30,7 @@ class EditUserController extends Controller
      */
     public function update(Request $request)
     {
-        // 1. 入力内容のバリデーション
+        // 入力内容のバリデーション
         $request->validate([
             'id' => ['required', 'integer', 'exists:users,id'],
             'name' => ['required', 'string', 'max:255'],
@@ -39,6 +40,7 @@ class EditUserController extends Controller
                 'max:255',
                 'unique:App\Models\User,login_name',
             ],
+            'icon' => ['image', 'mimes:jpeg,jpg,png,gif', 'max:2048']
             'permission' => ['required', 'integer', 'between:0,2'],
             'num_plan_std' => ['required', 'integer', 'between:0,4294967295'],
             'num_plan_prm' => ['required', 'integer', 'between:0,4294967295'],
@@ -49,7 +51,7 @@ class EditUserController extends Controller
 
         $user = User::find($request->input('id'));
 
-        // 2. 利用者モジュールを用いて利用者編集処理を実行
+        // 利用者モジュールを用いて利用者編集処理を実行
         $user->name = $request->input('name');
         $user->name = $request->input('login_name');
         $user->permission = $request->input('permission');
@@ -61,7 +63,23 @@ class EditUserController extends Controller
         $user->addr_city = $request->input('city');
         $user->addr_detail = $request->input('address');
 
-        // 保存
+        // アイコンが含まれていれば保存する
+        if($request->hasFile('icon')) {
+            $file = $request->file('icon');
+            if($file->isValid()) {
+                $this->storeIcon($user, $file);
+                // 拡張子の記録
+                $extension = $file->getClientOriginalExtension();
+                $user->icon_ext = $extension;
+            } else {
+                return redirect()
+                    ->back()
+                    ->withErrors(['error' => 'アイコンの保存に失敗しました。'])
+                    ->withInput();
+            }
+        }
+
+        // ユーザーデータ保存
         if ($user->save()) {
             // 3. 利用者一覧画面へのリダイレクトを含む応答
             return redirect()
@@ -74,5 +92,20 @@ class EditUserController extends Controller
             ->back()
             ->withErrors(['error' => '利用者情報の更新に失敗しました。'])
             ->withInput();
+    }
+
+    private function storeIcon(User $user, UploadedFile $file): void
+    {
+        // 古い画像があれば削除
+        if ($user->icon_ext) {
+            Storage::delete(
+                'public/icons/' . $user->id . '.' . $user->icon_ext,
+            );
+        }
+        $extension = $file->getClientOriginalExtension();
+        // ファイル名を「ユーザーID.拡張子」にする
+        $fileName = $user->id . '.' . $extension;
+        // storage/app/public/icons に保存
+        $file->storeAs('public/icons', $fileName);
     }
 }
