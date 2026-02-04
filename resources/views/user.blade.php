a208be947e507377b8dbe98d33575e2cfbf4009b
r02092 <87572333+r02092@users.noreply.github.com>
2026-02-05 03:35:53 +0900

    ユーザー追加画面を利用可能に

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,6 +5,11 @@
 @section('content')
 <h1>ユーザー一覧</h1>
 <section class="coupon-list">
+	<article class="general-box coupon-card" tabindex="0" role="article">
+		<button class="users-btn" onclick="location.href = '{{ route('admin.users.create') }}'">
+			ユーザーを追加
+		</button>
+	</article>
 	@foreach ($users as $user)
 	<article class="general-box coupon-card" tabindex="0" role="article" onclick="location.href='{{ route('admin.user.detail', ['id' => $user->id]) }}'" style="cursor: pointer;">
 		<img class="coupon-thumb" src="{{ $user->icon_url }}">
